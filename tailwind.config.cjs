25c5604a5af9babb1088ac26f0b74c8b189a34bc
Fernando Paiva <fernandosjp@gmail.com>
2024-04-13 01:56:05 -0300

    Change header color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,12 +5,14 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				"bg-header": "rgb(var(--color-bg-header))",
 				body: "rgb(var(--color-bg))",
+				"header": "rgb(var(--color-bg-header))",
 				"box-bg": "rgb(var(--box-bg))",
+				"box-bg-inverse": "rgb(var(--color-box-inverse)",
 				"box-shadow": "rgb(var(--box-sd))",
 				"box-border": "rgb(var(--box-border))",
 				"heading-1": "rgb(var(--heading-1))",
+				"heading-1-inverse": "rgb(var(--heading-1-inverse))",
 				"heading-2": "rgb(var(--heading-2))",
 				"heading-3": "rgb(var(--heading-3))",
 				secondary: "rgb(var(--secondary))",
